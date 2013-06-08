bcb95eac137691fc1d4e15cb9dbf185e04ec5c1a
Andy M <andy.dude@gmail.com>
2013-06-08 13:56:56 -0500

    Learning ruby the hard way exercises 1 - 5

---
+++ RubyTheHardWay/ex2.rb
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later
+# Anything after the # is ignored by Ruby.
+
+puts "I could have code like this." # and after the comment is ignored
+
+# You can also use a comment or disable a piece of code:
+# puts "this won't run"
+
+puts "This will run."
---
+++ RubyTheHardWay/ex3.rb
@@ -0,0 +1,33 @@
+puts "I will now count my chickens:"
+# 30/6 = 5.  
+# 5 + 25 = 30
+puts "Hens", 25+30/6
+# 25 * 3 = 75
+# 75 % 4 = 3
+# 100 - 3 = 97
+puts "Roosters", 100 - 25 * 3 % 4
+
+puts "Now I will count the eggs:"
+
+# 4 % 2 = 0
+# 1 / 4 = 0
+# 3 + 2 + 1 = 6
+# 6 - 5 = 1 
+# 1 + 0 + 0 + 6 = 7
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# 5 < -2 
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
